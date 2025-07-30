a662e6972eda93be41e7d5772dc6012c90b4550b
ma3oun <berjaoui@gmail.com>
2025-07-30 16:27:11 +0200

    Fixed import

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
 from .foretrieval import MultiModalRetrieverModel
+from .foretrieval.objects import Result
 
-__all__ = ["MultiModalRetrieverModel"]
+__all__ = ["MultiModalRetrieverModel", "Result"]
