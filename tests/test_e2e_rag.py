c3636626e46b509fe409d3836b30995ce85805ee
ma3oun <berjaoui@gmail.com>
2025-10-31 15:34:33 +0100

    dependencies update

--- tests/test_e2e_rag.py
+++ tests/test_e2e_rag.py
@@ -1,86 +1,77 @@
 from pathlib import Path
 from typing import Generator
-
+import shutil
 import pytest
 from colpali_engine.utils.torch_utils import get_torch_device, tear_down_torch
 
 from foretrieval import MultiModalRetrieverModel
 
-path_document_1 = Path("docs/attention.pdf")
-path_document_2 = Path("docs/attention_copy.pdf")
+path_document_1 = Path("sample_data/sample_pdf.pdf")
+path_document_2 = Path("sample_data/sample_multi_pdf.pdf")
+index_root = Path(".test_index")
 
 
 @pytest.fixture(scope="function")
 def rag_model_from_pretrained() -> Generator[MultiModalRetrieverModel, None, None]:
     device = get_torch_device("auto")
     print(f"Using device: {device}")
-    yield MultiModalRetrieverModel.from_pretrained("vidore/colpali-v1.2", device=device)
+    yield MultiModalRetrieverModel.from_pretrained(
+        "vidore/colqwen2.5-v0.2", device=device
+    )
     tear_down_torch()
 
 
 @pytest.fixture(scope="function")
 def rag_model_from_index() -> Generator[MultiModalRetrieverModel, None, None]:
-    yield MultiModalRetrieverModel.from_index("multi_doc_index")
+    if index_root.exists():
+        # delete the folder
+        shutil.rmtree(index_root)
+    yield MultiModalRetrieverModel.from_index(
+        "multi_doc_index", index_root=index_root.as_posix()
+    )
     tear_down_torch()
 
 
 @pytest.mark.slow
 def test_single_pdf(rag_model_from_pretrained: MultiModalRetrieverModel):
-    if not Path("docs/attention.pdf").is_file():
-        raise FileNotFoundError(
-            f"Please download the PDF file from https://arxiv.org/pdf/1706.03762 and move it to {path_document_1}."
-        )
-
     # Index a single PDF
     rag_model_from_pretrained.index(
-        input_path="docs/attention.pdf",
-        index_name="attention_index",
+        input_path=path_document_2,
+        index_name="sample_multipage_index",
         store_collection_with_index=True,
         overwrite=True,
     )
 
     # Test retrieval
     queries = [
-        "How does the positional encoding thing work?",
-        "what's the BLEU score of this new strange method?",
+        "What is the answer we are looking for on the first page?",
+        "What is the answer we are looking for on the second page?",
+        "What is the answer we are looking for on the third page?",
     ]
 
+    expected_page = 1
     for query in queries:
-        results = rag_model_from_pretrained.search(query, k=3)
+        result = rag_model_from_pretrained.search(query, k=1)[0]
 
         print(f"\nQuery: {query}")
-        for result in results:
-            print(
-                f"Doc ID: {result.doc_id}, Page: {result.page_num}, Score: {result.score}"
-            )
+        print(
+            f"Doc ID: {result.doc_id}, Page: {result.page_num}, Score: {result.score}"
+        )
 
-        # Check if the expected page (6 for positional encoding) is in the top results
-        if "positional encoding" in query.lower():
-            assert any(r.page_num == 6 for r in results), (
-                "Expected page 6 for positional encoding query"
-            )
+        # Check if the expected page is in the top results
 
-        # Check if the expected pages (8 and 9 for BLEU score) are in the top results
-        if "bleu score" in query.lower():
-            assert any(r.page_num in [8, 9] for r in results), (
-                "Expected pages 8 or 9 for BLEU score query"
-            )
+        assert result.page_num == expected_page, (
+            f"Expected page {expected_page} for this query. Got {result.page_num} instead"
+        )
+
+        expected_page += 1
 
 
 @pytest.mark.slow
 def test_multi_document(rag_model_from_pretrained: MultiModalRetrieverModel):
-    if not Path("docs/attention.pdf").is_file():
-        raise FileNotFoundError(
-            f"Please download the PDF file from https://arxiv.org/pdf/1706.03762 and move it to {path_document_1}."
-        )
-    if not Path("docs/attention_copy.pdf").is_file():
-        raise FileNotFoundError(
-            f"Please download the PDF file from https://arxiv.org/pdf/1706.03762 and move it to {path_document_2}."
-        )
-
     # Index a directory of documents
     rag_model_from_pretrained.index(
-        input_path="docs/",
+        input_path="sample_data/",
         index_name="multi_doc_index",
         store_collection_with_index=True,
         overwrite=True,
@@ -88,66 +79,18 @@ def test_multi_document(rag_model_from_pretrained: MultiModalRetrieverModel):
 
     # Test retrieval
     queries = [
-        "How does the positional encoding thing work?",
-        "what's the BLEU score of this new strange method?",
+        "What is the numerical answer we are looking for in the sample xls file?",
+        "What is the numerical answer we are looking for on the third page of the sample multipage pdf file?",
     ]
 
     for query in queries:
-        results = rag_model_from_pretrained.search(query, k=5)
+        result = rag_model_from_pretrained.search(query, k=1)[0]
 
         print(f"\nQuery: {query}")
-        for result in results:
-            print(
-                f"Doc ID: {result.doc_id}, Page: {result.page_num}, Score: {result.score}"
-            )
+        print(
+            f"Doc ID: {result.doc_id}, Page: {result.page_num}, Score: {result.score}"
+        )
 
-        # Check if the expected page (6 for positional encoding) is in the top results
-        if "positional encoding" in query.lower():
-            assert any(r.page_num == 6 for r in results), (
-                "Expected page 6 for positional encoding query"
-            )
-
-        # Check if the expected pages (8 and 9 for BLEU score) are in the top results
-        if "bleu score" in query.lower():
-            assert any(r.page_num in [8, 9] for r in results), (
-                "Expected pages 8 or 9 for BLEU score query"
-            )
-
-
-@pytest.mark.skip("This test should be made independent of the previous tests.")
-@pytest.mark.slow
-def test_add_to_index(rag_model_from_index: MultiModalRetrieverModel):
-    # NOTE: This test should run after the test_multi_document test.
-
-    # Add a new document to the index
-    rag_model_from_index.add_to_index(
-        input_item="docs/",
-        store_collection_with_index=True,
-        doc_id=[1002, 1003],
-        metadata=[{"author": "John Doe", "year": 2023}] * 2,
-    )
-
-    # Test retrieval with the updated index
-    queries = ["what's the BLEU score of this new strange method?"]
-
-    for query in queries:
-        results = rag_model_from_index.search(query, k=3)
-
-        print(f"\nQuery: {query}")
-        for result in results:
-            print(
-                f"Doc ID: {result.doc_id}, Page: {result.page_num}, Score: {result.score}"
-            )
-            print(f"Metadata: {result.metadata}")
-
-        # Check if the expected page (6 for positional encoding) is in the top results
-        if "positional encoding" in query.lower():
-            assert any(r.page_num == 6 for r in results), (
-                "Expected page 6 for positional encoding query"
-            )
-
-        # Check if the expected pages (8 and 9 for BLEU score) are in the top results
-        if "bleu score" in query.lower():
-            assert any(r.page_num in [8, 9] for r in results), (
-                "Expected pages 8 or 9 for BLEU score query"
-            )
+        # Check if the expected page (3) is in the top results
+        if "third" in query.lower():
+            assert result.page_num == 3, "Expected page 3 for multi-page pdf query"
